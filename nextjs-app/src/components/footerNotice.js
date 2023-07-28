36114281194cbdf57177d9a1ef8aa4febe7ea342
BODYFRIEND0\jm91 <jm91@bodyfriend.co.kr>
2023-07-28 10:20:34 +0900

    fix : icon check

--- nextjs-app/src/components/footerNotice.js
+++ nextjs-app/src/components/footerNotice.js
@@ -9,7 +9,7 @@ const FooterNotice = () => {
                     <span className="board-name">공지사항</span>
                     <span className="board-title">
                         <Link href="/" title="" className="new">
-                            서비스 이용약관 및 개인정보 처리방침 개정 안내
+                            서비스 이용약관 및 개인정보 처리방침 개정 안내 test
                         </Link>
                     </span>
                 </div>
